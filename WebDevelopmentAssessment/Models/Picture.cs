f5a33d1e01fa1e7f42ea7410a44c13df42d56c15
DJ Pownall <ac107151@avcol.school.nz>
2021-02-24 14:06:28 +1300

    Text validation and stylistic modifications

--- WebDevelopmentAssessment/Models/Picture.cs
+++ WebDevelopmentAssessment/Models/Picture.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,7 +10,11 @@ namespace WebDevelopmentAssessment.Models
     public class Picture
     {
         public int ID { get; set; }
+        [StringLength(30, MinimumLength = 5)]
         public string Title { get; set; }
+        [RegularExpression("^http")]
+        [StringLength(64)]
+        [Required]
         public string ImageURL { get; set; }
         public User Author { get; set; }
         public ICollection<Tag> Tags { get; set; }
