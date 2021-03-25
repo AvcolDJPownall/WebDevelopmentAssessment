6686c3ca5c7008480854c3504443592670495fb6
DJ Pownall <ac107151@avcol.school.nz>
2021-03-26 10:42:36 +1300

    Primative tag page

---
+++ WebDevelopmentAssessment/Pages/Tags/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WebDevelopmentAssessment.Data;
+using WebDevelopmentAssessment.Models;
+
+namespace WebDevelopmentAssessment.Pages.Tags
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly WebDevelopmentAssessment.Data.WebContentContext _context;
+
+        public DetailsModel(WebDevelopmentAssessment.Data.WebContentContext context)
+        {
+            _context = context;
+        }
+
+        public Tag Tag { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Tag = await _context.Tags.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Tag == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
---
+++ WebDevelopmentAssessment/Pages/Tags/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WebDevelopmentAssessment.Data;
+using WebDevelopmentAssessment.Models;
+
+namespace WebDevelopmentAssessment.Pages.Tags
+{
+    public class IndexModel : PageModel
+    {
+        private readonly WebDevelopmentAssessment.Data.WebContentContext _context;
+
+        public IndexModel(WebDevelopmentAssessment.Data.WebContentContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Tag> Tag { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Tag = await _context.Tags.ToListAsync();
+        }
+    }
+}
